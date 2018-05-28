7910c641001fb0059ee1cdf8ead94af72a670ff4
Jack Nehme <jack.nehme@gmail.com>
2018-05-27 22:27:19 -0700

    init commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Employee } from '../models/employee.model';
+
+@Component({
+  selector: 'home',
+  styleUrls: ['./home.component.css'],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+  languages = ['English', 'Spanish', 'Other',];
+  model = new Employee('', '', true, '', 'default');
+  hasPrimaryLanguageError = false;
+
+  validatePrimaryLanguage(value) {
+    if(value === 'default'){
+      this.hasPrimaryLanguageError = true;
+    } else {
+      this.hasPrimaryLanguageError = false;
+    }
+  }
+
+}
